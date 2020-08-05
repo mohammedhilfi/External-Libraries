85d6a86fe24c733b1dff2e6e1c57261b3dc9393a
mohammedhilfi <momohilfi03@Gmail.com>
2020-08-05 16:11:16 -0700

    charts

--- chart.js
+++ chart.js
@@ -4,7 +4,7 @@ let numberArray = [];
 const nameInput = document.getElementById('nameInput');
 const numberInput = document.getElementById('numberInput');
 const enterButton = document.getElementById('button');
-let ctx = document.getElementById('chart')
+
 
 enterButton.addEventListener('click' , function (e) {
 e.preventDefault();
@@ -13,44 +13,10 @@ numberArray.push(numberInput.value);
 nameInput.value = '';
 numberInput.value = 0;
 if (nameArray.length >= 5 && numberArray.length >= 5){
-
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: nameArray,
-            datasets: [{
-                label: '# of Votes',
-                data: numberArray,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
-    });
+localStorage.setItem('nameArray', JSON.stringify(nameArray) )
+localStorage.setItem('numberArray', JSON.stringify(numberArray) )
+// localStorage.nameArray = JSON.stringify(nameArray)
+// localStorage.numberArray = JSON.stringify(numberArray)
 }
-
 });
 
---
+++ chart2.js
@@ -0,0 +1,40 @@
+let ctx = document.getElementById('chart')
+let nameArray = JSON.parse(localStorage.nameArray)
+let numberArray = JSON.parse(localStorage.numberArray)
+console.log(numberArray);
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: nameArray,
+        datasets: [{
+            label: '# of Votes',
+            data: numberArray,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
